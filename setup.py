fa06e43279e58ef7dc9f31c127a1da5fc6fba050
Richard Darst <rkd@zgib.net>
2019-06-18 02:06:12 +0300

    New organization: NordicHPC

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Jupyter kernels in docker, singularity, Lmod",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/AaltoScienceIT/envkernel",
+    url="https://github.com/NordicHPC/envkernel",
     #packages=setuptools.find_packages(),
     py_modules=["envkernel"],
     entry_points={
